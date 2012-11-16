3bcf6c2ff7e5ae26c2b8eb773ab1a80fff5409f6
Noah Freitas <noah.j.freitas@gmail.com>
2012-11-15 20:20:56 -0800

    Turned zip/2 and lenient_zip/2 into tail recursive version.

--- recursion/recursion.erl
+++ recursion/recursion.erl
@@ -1,9 +1,21 @@
 -module(recursion).
 -export([zip/2, lenient_zip/2]).
 
-zip([],[]) -> [];
-zip([X|Xs],[Y|Ys]) -> [{X,Y}|zip(Xs,Ys)].
 
-lenient_zip([],_) -> [];
-lenient_zip(_,[]) -> [];
-lenient_zip([X|Xs],[Y|Ys]) -> [{X,Y}|lenient_zip(Xs,Ys)].
+% Zips two lists of equal length into a list of two
+% element tuples.
+% Turned non-tail recursive code from website into
+% tail-recursive version.
+zip(X,Y) -> zip(X,Y,[]).
+zip([],[],Acc) -> lists:reverse(Acc);
+zip([X|Xs],[Y|Ys],Acc) -> zip(Xs,Ys,[{X,Y}|Acc]).
+
+
+% Zips two lists of variable length into a list of
+% two element tuples.
+% Turned non-tail recursive code from website into
+% tail-recursive version.
+lenient_zip(X,Y) -> lenient_zip(X,Y,[]).
+lenient_zip([],_,Acc) -> lists:reverse(Acc);
+lenient_zip(_,[],Acc) -> lists:reverse(Acc);
+lenient_zip([X|Xs],[Y|Ys],Acc) -> lenient_zip(Xs,Ys,[{X,Y}|Acc]).
